c022977f33363936dcdb753fb5e8a0847eaccb4d
Brian C Sparks <brian.c.sparks@hp.com>
2020-03-08 18:31:29 -0700

    with piggybacked bot

--- Big_Boy_Points/main.c
+++ Big_Boy_Points/main.c
@@ -1,4 +1,8 @@
 #include <kipr/botball.h>
+/** things to do for 3/14:
+template
+2nd half
+make claw sturdier**/
 void straight(speed, time){
 	create_drive_straight(-speed);
     msleep(time);
@@ -28,18 +32,26 @@ int main()
 {
     create_connect();
     printf("Connected\n");
-    straight(250, 1500);
-    hard_left(1000, 1000);
+    enable_servos();
+    set_servo_position(3, 2047);
+    straight(250, 2150);
+    stop(12000);
+    //straight(-100, 500);
+    //stop(3000);
+    //hard_left(150, 2500);
+    //stop(3000);
+    set_servo_position(3, 1400);
     //wait for robot to go out
-    stop(1000);
-    straight(-250, 500);
-    spin_left(250, 800);
-    straight(250, 1500);
-    //add servo
-   	spin_right(250, 1000);
-    straight(250, 1000);
+    //stop(1000);
+    //straight(-150, 500);
+    //spin_right(250, 600);
+    straight(-250, 1750);
+   	spin_left(250, 500);
+    set_servo_position(3, 2047);
+    straight(250, 2400);
+    spin_right(100, 1000);
+    spin_left(100, 550);
     create_disconnect();
     printf("Disconnected\n");
     return 0;
 }
-
